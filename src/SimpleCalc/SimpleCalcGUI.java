b5ebbda4fdfe3b5456ad95b9c475886e6b580980
idul <ludivicobalaterojr@gmail.com>
2022-12-14 12:50:22 +0800

    Rename SimpleCalc components Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,8 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -11,9 +13,20 @@ public class SimpleCalcGUI extends JFrame{
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+    SimpleCalcGUI(){
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(tfNumber1.getText());
+                int num2 = Integer.parseInt(tfNumber2.getText());
 
+            }
+        });
+    }
     public static void main(String[] args) {
+
         SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setTitle("Simple Calculator");
         app.setContentPane(app.panel1);
         app.setSize(500,400);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
